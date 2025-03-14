0888df762be64bd551a68790337e010fd89703f0
Nguyen Dang Hoang Giang <gianghoang150503@gmail.com>
2025-03-14 11:34:58 +0700

    Updated

---
+++ src/pages/products/[id]/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+import LayoutUser from '@/components/layouts/LayoutUser/LayoutUser';
+import {Fragment} from 'react';
+
+export default function Page() {
+	return (
+		<Fragment>
+			<Head>
+				<title>Chi tiết sản phẩm</title>
+				<meta name='description' content='Chi tiết sản phẩm' />
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<Fragment>
+				<div>Chi tiết sản phẩm</div>
+			</Fragment>
+		</Fragment>
+	);
+}
+
+Page.getLayout = function (page) {
+	return <LayoutUser>{page}</LayoutUser>;
+};
