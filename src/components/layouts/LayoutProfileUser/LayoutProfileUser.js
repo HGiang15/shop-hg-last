7f38f8cf6cc6e7747b5b35df89ae6b440b4ff7a9
Nguyen Dang Hoang Giang <gianghoang150503@gmail.com>
2025-04-02 10:55:34 +0700

    Update Breadcrumb

--- src/components/layouts/LayoutProfileUser/LayoutProfileUser.js
+++ src/components/layouts/LayoutProfileUser/LayoutProfileUser.js
@@ -3,7 +3,10 @@ import Breadcrumb from '@/components/common/Breadcrumb/Breadcrumb';
 import styles from './LayoutProfileUser.module.scss';
 import SidebarProfile from './SidebarProfile/SidebarProfile';
 
-const LayoutProfileUser = ({children, breadcrumbItems}) => {
+const LayoutProfileUser = ({children, breadcrumbItems = {titles: [], listHref: []}}) => {
+	if (!breadcrumbItems.titles || !breadcrumbItems.listHref) {
+		return <div>Invalid breadcrumb data</div>;
+	}
 	return (
 		<div className={styles.container}>
 			{/* Nhận breadcrumbItems từ props và truyền vào Breadcrumb */}
--- src/components/pages/product/MainPageProduct/MainPageProduct.js
+++ src/components/pages/product/MainPageProduct/MainPageProduct.js
@@ -4,7 +4,11 @@ import Breadcrumb from '@/components/common/Breadcrumb/Breadcrumb';
 import FilterProduct from '../FilterProduct/FilterProduct';
 import ProductCard from '../ProductCard/ProductCard';
 
-const MainPageProduct = ({breadcrumbItems}) => {
+const MainPageProduct = ({breadcrumbItems = {titles: [], listHref: []}}) => {
+	if (!breadcrumbItems.titles || !breadcrumbItems.listHref) {
+		// Trường hợp không có breadcrumb
+		return <div>Invalid breadcrumb data</div>;
+	}
 	return (
 		<div className={styles.container}>
 			{/* Nhận breadcrumbItems từ props và truyền vào Breadcrumb */}
