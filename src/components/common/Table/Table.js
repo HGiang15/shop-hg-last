1b34c795c95fd774abe6430551683a3a925f8e82
Nguyen Dang Hoang Giang <gianghoang150503@gmail.com>
2025-04-28 09:10:26 +0700

    Updated Table component

--- src/components/common/Table/Table.js
+++ src/components/common/Table/Table.js
@@ -1,36 +1,7 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React from 'react';
 import styles from './Table.module.scss';
-import {ChromePicker} from 'react-color';
 
 function Table({users, headers, renderActions, roleStyle, statusStyle}) {
-	const [selectedColors, setSelectedColors] = useState({});
-	const [showColorPickers, setShowColorPickers] = useState({});
-
-	const handleColorChange = (color, id) => {
-		setSelectedColors({...selectedColors, [id]: color.hex});
-	};
-
-	const handleColorClick = (id) => {
-		setShowColorPickers({...showColorPickers, [id]: !showColorPickers[id]});
-	};
-
-	const pickerRefs = useRef({});
-
-	useEffect(() => {
-		const handleClickOutside = (event) => {
-			Object.keys(showColorPickers).forEach((id) => {
-				if (showColorPickers[id] && pickerRefs.current[id] && !pickerRefs.current[id].contains(event.target)) {
-					setShowColorPickers({...showColorPickers, [id]: false});
-				}
-			});
-		};
-
-		document.addEventListener('mousedown', handleClickOutside);
-		return () => {
-			document.removeEventListener('mousedown', handleClickOutside);
-		};
-	}, [showColorPickers]);
-
 	return (
 		<div className={styles.tableWrapper}>
 			<table className={styles.table}>
@@ -45,47 +16,17 @@ function Table({users, headers, renderActions, roleStyle, statusStyle}) {
 				<tbody>
 					{users.map((user, index) => (
 						<tr key={`${user.id}-${index}`}>
-							{headers.map((header) => {
-								if (header.key === 'colorCode') {
-									return (
-										<td key={header.key}>
-											<div className={styles.colorPickerContainer}>
-												<div
-													className={styles.colorDisplay}
-													style={{
-														backgroundColor: selectedColors[user.id] || '#ff0000',
-													}}
-													onClick={() => handleColorClick(user.id)}
-												></div>
-												{showColorPickers[user.id] && (
-													<div
-														className={styles.chromePickerWrapper}
-														ref={(el) => (pickerRefs.current[user.id] = el)}
-													>
-														<ChromePicker
-															color={selectedColors[user.id] || '#ff0000'}
-															onChange={(color) => handleColorChange(color, user.id)}
-														/>
-													</div>
-												)}
-												<p className={styles.colorHex}>{selectedColors[user.id] || '#ff0000'}</p>
-											</div>
-										</td>
-									);
-								} else {
-									return (
-										<td key={header.key}>
-											{header.key === 'role' ? (
-												<span style={roleStyle}>{user[header.key]}</span>
-											) : header.key === 'status' ? (
-												<span style={statusStyle}>{user[header.key]}</span>
-											) : (
-												user[header.key]
-											)}
-										</td>
-									);
-								}
-							})}
+							{headers.map((header) => (
+								<td key={header.key}>
+									{header.key === 'role' ? (
+										<span style={roleStyle}>{user[header.key]}</span>
+									) : header.key === 'status' ? (
+										<span style={statusStyle}>{user[header.key]}</span>
+									) : (
+										user[header.key]
+									)}
+								</td>
+							))}
 							<td>
 								<div className={styles.actions}>{renderActions(user)}</div>
 							</td>
